922ace2671e9abde6843a04f096c5511af7e74bd
Huy Luong <huyluong@huys-mbp.attlocal.net>
2016-11-21 22:25:35 -0500

    another update

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 
 
 $(function () {
-  $('.main ul').hide();
+  $('.files').hide();
   $('.menu').click(function() {
     $('.files').slideUp(500);
     $(this).find('.files').show()
    });
 });
+ // $('.main ul').hide();
 // $(this).find('ul').show()
